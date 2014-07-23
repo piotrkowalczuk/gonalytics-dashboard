7b8835854265283e691e6b938c71ee4cb43880e8
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2014-07-24 01:08:06 +0200

    Dashboard visits live, visits graph

--- application/dashboard/views.py
+++ application/dashboard/views.py
@@ -3,13 +3,18 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic.base import View
 from django.shortcuts import render
 from datetime import date
+import jsondatetime as json
 import requests
+import json
 
 class Dashboard(View):
 
     @method_decorator(login_required)
     def get(self, request):
         dateTimeRange = request.GET.get('dateTimeRange', date.today().strftime("%Y-%m-%d-day"))
+
+        visitsLive = requests.get('http://localhost:8080/visits/live')
+        visits = requests.get('http://localhost:8080/visits?dateTimeRange='+dateTimeRange)
 
         nbOfVisits = requests.get('http://localhost:8080/visits/count?dateTimeRange='+dateTimeRange)
         nbOfActions = requests.get('http://localhost:8080/visits/actions/count?dateTimeRange='+dateTimeRange)
@@ -19,9 +24,15 @@ class Dashboard(View):
             request,
             'dashboard/show.html',
             {
+                'visits': json.loads(json.dumps(visits.text, default=dthandler)),
+                'visitsLive': visitsLive.json(),
                 'nbOfVisits': nbOfVisits.json(),
                 'nbOfActions': nbOfActions.json(),
                 'nbOfCountries': nbOfCountries.json(),
                 'today': date.today(),
             }
         )
+dthandler = lambda obj: (
+    obj.isoformat()
+    if isinstance(obj, datetime.datetime) or isinstance(obj, datetime.date)
+    else None)
